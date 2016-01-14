733c664aae228ce553ca3749f280099a552752fd
符现超 <fuxianchao@gmail.com>
2016-01-14 14:30:53 +0800

    改为静态cell,减少code

--- ZDCALayerDemo/Controllers/RootController/RootViewController.m
+++ ZDCALayerDemo/Controllers/RootController/RootViewController.m
@@ -10,8 +10,6 @@
 
 @interface RootViewController ()
 
-@property (nonatomic, copy) NSArray *dataSource;
-
 @end
 
 @implementation RootViewController
@@ -27,48 +25,14 @@
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    
 }
 
-#pragma mark - UITableViewDataSource && UITableViewDelegate
+#pragma mark - Property
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return self.dataSource.count;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"cellIdentifier";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (!cell)
-    {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-        cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    }
-    cell.textLabel.text = self.dataSource[indexPath.row];
-    return cell;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    return UITableViewAutomaticDimension;
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    [self.navigationController pushViewController:[NSClassFromString(@"CAShapeLayerController") new] animated:YES];
-}
-
-#pragma mark - Getter
-- (NSArray *)dataSource
-{
-    return @[@"CAShapeLayer", @"CABasic"];
-}
 
 @end
+
+
+
+
