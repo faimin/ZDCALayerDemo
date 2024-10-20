478f0f15960ba53d456fdd466c4f05a5c5c20251
faimin <fuxianchao@gmail.com>
2024-10-20 21:54:11 +0800

    ellipsis animation

--- ZDCALayerDemo/Controllers/CAReplicatorLayer/CAReplicatorLayerController.m
+++ ZDCALayerDemo/Controllers/CAReplicatorLayer/CAReplicatorLayerController.m
@@ -29,9 +29,11 @@
 }
 
 - (void)setup {
-    [self setupUI];
+    //[self setupUI];
+    [self setuptailwaveAnimation];
 }
 
+// 音乐千千静听效果
 - (void)setupUI {
     NSUInteger count = 5;
     CGFloat subLayerWidth = 40.0, transformX = 60.0, totoalWidth = transformX*count-(transformX-subLayerWidth);
@@ -60,6 +62,8 @@
     CABasicAnimation *animation = [CABasicAnimation animation];
     animation.keyPath = @"transform.scale.y";
     animation.toValue = @0.05;
+    //animation.keyPath = @"transform.translation.y";
+    //animation.toValue = @(600);
     animation.duration = 0.5;
     animation.autoreverses = YES;
     animation.repeatCount = MAXFLOAT;
@@ -69,14 +73,48 @@
     [animationView.layer addSublayer:replacatorLayer];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+// 省略号波浪动画
+- (void)setuptailwaveAnimation {
+    NSUInteger count = 3;
+    UIFont *font = [UIFont systemFontOfSize:24 weight:UIFontWeightMedium];
+    CGFloat width = 10.0;
+    CGFloat totoalWidth = width * count;
+    
+    UIView *animationView = [[UIView alloc] initWithFrame:CGRectMake(50, 100, totoalWidth, font.lineHeight * 1)];
+    animationView.backgroundColor = [UIColor cyanColor];
+    [self.view addSubview:animationView];
+    
+    CAReplicatorLayer *replacatorLayer = [CAReplicatorLayer layer];
+    replacatorLayer.frame = animationView.bounds;
+    replacatorLayer.instanceCount = count;
+    // 60 = 复制层宽度 + 复制层之间的间隔
+    replacatorLayer.instanceTransform = CATransform3DMakeTranslation(width, 0, 0);
+    replacatorLayer.instanceDelay = 0.1;
+    //replacatorLayer.instanceColor = UIColor.orangeColor.CGColor;
+    replacatorLayer.instanceBlueOffset = 0.1;
+    
+    // 创建复制层的sublayer
+    CATextLayer *textLayer = ({
+        CATextLayer *layer = [CATextLayer layer];
+        layer.string = @"·";
+        layer.font = (__bridge CFTypeRef)(font);
+        layer.fontSize = font.pointSize;
+        layer.foregroundColor = UIColor.grayColor.CGColor;
+        //layer.anchorPoint = CGPointMake(0.5, 0.5);
+        layer.frame = CGRectMake(0, 0, width, CGRectGetHeight(replacatorLayer.frame)/2);
+        layer;
+    });
+    
+    CABasicAnimation *animation = [CABasicAnimation animation];
+    animation.keyPath = @"transform.translation.y";
+    animation.toValue = @(CGRectGetHeight(replacatorLayer.bounds)/2.0);
+    animation.duration = 0.5;
+    animation.autoreverses = YES;
+    animation.repeatCount = MAXFLOAT;
+    
+    [textLayer addAnimation:animation forKey:@"CAReplicatorLayer"];
+    [replacatorLayer addSublayer:textLayer];
+    [animationView.layer addSublayer:replacatorLayer];
 }
-*/
 
 @end
